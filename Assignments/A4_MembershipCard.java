c92afe11e97c84b07bcd20c950a0750ddccfcb70
karthika6326-coder <karthika6326@gmail.com>
2026-09-05 01:46:20 +0530

    Add Week 4 assignment problems

---
+++ Assignments/A4_MembershipCard.java
@@ -0,0 +1,27 @@
+public class A4_MembershipCard {
+
+    static class MembershipCard {
+        static String libraryName;
+        static String validUntil;
+        String studentName;
+
+        static {
+            libraryName = "SRM Central Library";
+            validUntil = "May 2027";
+            System.out.println("Library info loaded");
+        }
+
+        public MembershipCard(String studentName) {
+            this.studentName = studentName;
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] names = {"Ananya", "Rohan", "Priya", "Arjun", "Sneha"};
+
+        for (String name : names) {
+            MembershipCard card = new MembershipCard(name);
+            System.out.println("Membership card issued: " + card.studentName);
+        }
+    }
+}
